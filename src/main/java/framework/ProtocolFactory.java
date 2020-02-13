747cc5f96daf1676d803ddc92a60923f854f16db
dyywinner <dyywinner@126.com>
2020-02-14 00:21:17 +0800

    spi feature

--- src/main/java/framework/ProtocolFactory.java
+++ src/main/java/framework/ProtocolFactory.java
@@ -3,18 +3,30 @@ package framework;
 import protocol.netty.NettyProtocol;
 import protocol.http.HttpProtocol;
 
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
 public class ProtocolFactory {
-    // 工厂模式
+
     public static Protocol getProtocol(){
-        String name = System.getProperty("protocolName");
-        if(name == null || name.equals("")) {
-            name = "http";
-        }
-        switch(name){
-            case "http": return new HttpProtocol();
-            case "netty": return new NettyProtocol();
-            default: break;
-        }
-        return new HttpProtocol();
+        // Java spi
+        ServiceLoader<Protocol> serviceLoader = ServiceLoader.load(Protocol.class);
+        Iterator<Protocol> iterator = serviceLoader.iterator();
+        return iterator.next();
     }
+
+
+// 工厂模式
+//    public static Protocol getProtocol(){
+//        String name = System.getProperty("protocolName");
+//        if(name == null || name.equals("")) {
+//            name = "http";
+//        }
+//        switch(name){
+//            case "http": return new HttpProtocol();
+//            case "netty": return new NettyProtocol();
+//            default: break;
+//        }
+//        return new HttpProtocol();
+//    }
 }
